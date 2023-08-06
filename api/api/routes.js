467364c1d10abac9616933b6ce886df5d2be3e70
Geraldo Marques <108906049+GeraldoMarques@users.noreply.github.com>
2023-08-06 00:28:24 -0300

    Email back-end solving conflicts

--- api/api/routes.js
+++ api/api/routes.js
@@ -4,15 +4,17 @@ const userController = require("./controller/userController");
 const companyController = require("./controller/companyController")
 const discarddController = require("./controller/discarddController")
 const authUserController = require("./controller/authUserController")
+const authCompanyController = require("./controller/authCompanyController")
 const maillingController = require("./controller/maillingController");
-
 
 const router = express.Router();
 router.post("/users", userController.addUser);
-router.post("/company", companyController.addCompany)
-router.post("/discardd", discarddController.addDiscardd)
-router.post("/login", authUserController.loginUser)
+router.post("/company", companyController.addCompany);
+router.post("/discardd", discarddController.addDiscardd);
+router.post("/login", authUserController.loginUser);
+router.post("/loginCompany", authCompanyController.loginCompany);
 router.post("/mail", maillingController.sendMail);
+
 
 
 module.exports = router;
